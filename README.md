b466f66f0499a7b929f41cd2e7e1348b388312a6
Joshua Martinez <joshkmartinez@gmail.com>
2019-04-01 16:54:43 -0700

    I actually made it...

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# emoji
+# Emoji
+
+This is a microservice that utilizes [joypixels](https://www.joypixels.com/) to give you an emoji!
+Check it out at [emoji.now.sh](https://emoji.now.sh)
---
+++ api.js
@@ -0,0 +1,48 @@
+var express = require('express')
+var app = express()
+const Emoji = require('emojione')
+const api = express.Router()
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  )
+  next()
+})
+//order matters
+api.get('/list', (req, res) => {
+  res.sendFile('./emoji.json', { root: __dirname })
+})
+
+api.get('/:emoji', (req, res) => {
+  const emoji = req.params.emoji
+
+  const image = Emoji.shortnameToImage(`:${emoji}:`)
+  const re = /png\/(.*).png/
+  try {
+    image.match(re)[1]
+  } catch (e) {
+    return res.send('emoji not found')
+  }
+  const found = image.match(re)[1]
+  let filePath = `./node_modules/emojione/assets/png/${found}.png`
+
+  return res.sendFile(filePath, { root: __dirname })
+})
+api.get('/', (req, res) => {
+  res.send(
+    `
+    Endpoints:
+    GET /list
+    GET /:emojiName
+    
+    e.g. GET /smirk
+    github.com/joshkmartinez/emoji
+    `
+  )
+})
+app.use('/', api)
+app.listen(3000, () => {
+  //console.log('Server running on port 3000')
+})
